b754e76a87b47a6f6fc66be2f52ecd452468dde4
swryan <stephen.w.ryan@gmail.com>
2016-02-11 22:42:54 -0500

    runs benchmarks, writes data to database

--- benchmark.py
+++ benchmark.py
@@ -1,232 +1,301 @@
-#!/hx/u/kmarstel/anaconda/bin/python
+#!python
+from __future__ import print_function
 
-from subprocess import call, Popen, PIPE
-import os, shlex, sqlite3, yaml, time, csv
+from subprocess import Popen, PIPE
+
+import sys
+import os
+import shlex
+import sqlite3
+import time
+import json
+import csv
+
+from argparse import ArgumentParser
+
 from contextlib import contextmanager
 
-def benchmark(project):
-    update_triggered_by = []
-    benchmark_needed = 0
-    project_dir = "%s%s" % (os.path.expanduser("~/"), project)
+# default configuration options
+conf = {
+    "working_dir": ".",
+    "repo_dir": "."
+}
 
-    with cd(project_dir):
-        #connect to database, prepare to execute cmds with cursor
-        dbname = "benchmark.db"
-        conn = sqlite3.connect(dbname)
-        c = conn.cursor()
 
-        #open yaml config file, parse it.
-        with open("benchmark.yaml", 'r') as stream:
-            yaml_data = yaml.load(stream)
-        
-        #check local project for latest commit
-        last_benched_commit_project = get_last_benchmarked_commit(c, project) 
-        current_commit_project = get_current_commit_project(project, str(yaml_data["branch"]))
-        if (last_benched_commit_project != current_commit_project):
-            update_triggered_by.append(project)
-            benchmark_needed = 1 
-         
-        for dependency in yaml_data["dependencies"]:
-            dep_name = dependency.split('/')[-1]
-            print(dep_name)
-            last_benched_commit = str(get_last_benchmarked_commit(c, dependency))
-            print ("Last Benched Commit: " + last_benched_commit)
-            current_commit = str(get_current_commit_deps(dependency))
-            print ("Current Commit: " + current_commit)
-            if (last_benched_commit != current_commit):
-                print("%s is out of date.\n\n" % dep_name)
-                update_triggered_by.append(dependency)
-                benchmark_needed = 1
+class BenchmarkDatabase(object):
+    def __init__(self, name):
+        self.dbname = name+".db"
+        self.conn   = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
 
-        if (benchmark_needed):
-            print("Benchmark triggered by updates to: ", update_triggered_by)
-            conda_env = create_conda_env(project, yaml_data["dependencies"])
-            activate_install_conda_env(conda_env, yaml_data["dependencies"])
-            run_testflo_benchmarks()
-            read_benchmark_data(c)
-            remove_conda_env(conda_env)
+    def get_last_commit(self, dependency):
+        """
+        Check the database for the most recent commit that was benchmarked.
+        """
+        cursor = self.cursor
 
-        conn.commit()
-        conn.close()
+        # if the commits table has not been created yet, create it
+        cursor.execute("CREATE TABLE if not exists Commits (Dependency TEXT, Repo TEXT, LastCommit TEXT)")
 
-@contextmanager
-def cd(newdir):
+        # try to
+        execute_string = "SELECT LastCommit FROM Commits WHERE Repo == "
+        execute_string += "'" + dependency + "'"
+        cursor.execute(execute_string)
+        commit = cursor.fetchall()
+        print("last commit for", dependency, "=", commit)
+        return commit
+
+    def add_benchmark_data(self, filename):
+        """
+        Stolen directly from Steve, needs to be modified
+        """
+        cursor = self.cursor
+        cursor.execute("CREATE TABLE if not exists BenchmarkData (DateTime INT, Spec TEXT, Status TEXT, Elapsed REAL, Memory REAL)")
+        with open(filename, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                cursor.execute("INSERT INTO BenchmarkData VALUES(?, ?, ?, ?, ?)", (row[0], row[1], row[2], row[3], row[4]))
+
+    def dump_benchmark_data(self):
+        with open(self.dbname+'.benchmark_data.sql', 'w') as f:
+            for line in self.conn.iterdump():
+                f.write('%s\n' % line)
+
+
+def read_json(filename):
     """
-    A cd that will better handle error and return to its orig dir.
+    read data from a JSON file
     """
-    prevdir = os.getcwd()
-    os.chdir(os.path.expanduser(newdir))
     try:
-        yield
-    finally:
-        os.chdir(prevdir)
+        with open(filename) as json_file:
+            return json.loads(json_file.read())
+    except:
+        raise RuntimeError("%s is not a valid JSON file." % filename)
 
-#Currently using this to get debug information on calls
+
 def get_exitcode_stdout_stderr(cmd):
     """
     Execute the external command and get its exitcode, stdout and stderr.
     """
+    #Currently using this to get debug information on calls
+    print('CMD:', cmd)
     args = shlex.split(cmd)
     proc = Popen(args, stdout=PIPE, stderr=PIPE)
     out, err = proc.communicate()
     exitcode = proc.returncode
     return exitcode, out, err
 
-def get_last_benchmarked_commit(cursor, dependency):
-    """
-    Check the database for the most recent commit that was benchmarked.
-    """
-    execute_string = "SELECT LatestCommit FROM DependencyCommits WHERE Repo == "
-    execute_string += "'" + dependency + "'"
-    cursor.execute(execute_string)
-    commit = cursor.fetchall()
-    return commit
 
-def get_current_commit_project(project, branch):
+@contextmanager
+def cd(newdir):
     """
-    Chcek the local project for it's latest commit.
+    A cd that will better handle error and return to its orig dir.
     """
-    #first check the project to see if it has changed.
-    git_pull = "git pull origin " + branch
-    code, out, err = get_exitcode_stdout_stderr(git_pull)
-    code, out, err = get_exitcode_stdout_stderr("git rev-parse HEAD")
+    print('cd into', newdir)
+    prevdir = os.getcwd()
+    fulldir = os.path.expanduser(newdir)
+    if not os.path.exists(fulldir):
+        os.makedirs(fulldir)
+    os.chdir(fulldir)
+    try:
+        yield
+    finally:
+        print('cd from', fulldir, 'back to', prevdir)
+        os.chdir(prevdir)
+
+
+@contextmanager
+def repo(repository):
+    """
+    cd into local copy of repository.  if the repository has not been
+    cloned yet, then clone it to working directory first.
+    """
+    prev_dir = os.getcwd()
+
+    repo_dir = os.path.expanduser(conf["repo_dir"])
+    if not os.path.exists(repo_dir):
+        os.makedirs(repo_dir)
+    print('cd into repo dir', repo_dir, 'from', prev_dir)
+    os.chdir(repo_dir)
+
+    repo_name = repository.split('/')[-1]
+    if not os.path.isdir(repo_name):
+        clone_repo(repository)
+
+    print('cd into repo', repo_name)
+    os.chdir(repo_name)
+    try:
+        yield
+    finally:
+        print('cd from repo', repo_name, 'back to', prev_dir)
+        os.chdir(prev_dir)
+
+
+def benchmark(project):
+    project_info = read_json(project+".json")
+    project_info["name"] = project
+
+    update_triggered_by = []
+    benchmark_needed = False
+
+    db = BenchmarkDatabase(project)
+
+    dependencies = project_info["dependencies"]
+    dependencies.append(project_info["repository"])
+
+    for dependency in dependencies:
+        with repo(dependency):
+            last_commit = str(db.get_last_commit(dependency))
+            print ("Last Commit: " + last_commit)
+            current_commit = get_current_commit()
+            print ("Current Commit: " + current_commit)
+            if (last_commit != current_commit):
+                print("There has been an update to %s.\n\n" % dependency)
+                update_triggered_by.append(dependency)
+                benchmark_needed = 1
+
+    if (benchmark_needed):
+        print("Benchmark triggered by updates to: ", update_triggered_by)
+        conda_env = create_conda_env(project)
+        activate_install_conda_env(conda_env, project_info["dependencies"])
+        with repo(project_info["repository"]):
+            get_exitcode_stdout_stderr("pip install -e .")
+            run_benchmarks()
+            db.add_benchmark_data("benchmark_data.csv")
+
+        db.dump_benchmark_data()
+        remove_conda_env(conda_env)
+
+
+def clone_repo(repository):
+    """
+    clone repository into current directory
+    """
+    clone_cmd = " clone " + repository
+
+    code, out, err = get_exitcode_stdout_stderr("git" + clone_cmd)
+    if (code != 0):
+        code, out, err = get_exitcode_stdout_stderr("hg" + clone_cmd)
+
+    print(code, out, err)
+
+    if code:
+        raise RuntimeError("Could not clone %s" % repository)
+
+
+def get_current_commit():
+    """
+    Update and check the current repo for the most recent commit.
+    """
+    pull_git = "git pull origin master"
+    pull_hg = "hg pull; hg merge"
+
+    commit_git = "git rev-parse HEAD"
+    commit_hg = "hg id -i"
+
+    # pull latest commit from desired branch and get the commit ID
+    code, out, err = get_exitcode_stdout_stderr(pull_git)
+    if (code is 0):
+        code, out, err = get_exitcode_stdout_stderr(commit_git)
+    else:
+        code, out, err = get_exitcode_stdout_stderr(pull_hg)
+        code, out, err = get_exitcode_stdout_stderr(commit_hg)
+
     return out
 
-def get_current_commit_deps(dependency):
+
+def create_conda_env(project):
     """
-    Update and check the local dep repo for the most recent commit.
-    """
-    with cd (os.path.expanduser("~")):
-        name = dependency.split('/')[-1]
-        repo_dir = "%s%s" % (os.path.expanduser("~/"), name)
-
-        clone_git = "git clone " + dependency
-        clone_hg = "hg clone " + dependency
-
-        pull_git = "git pull origin master"
-        pull_hg = "hg pull; hg merge"
-
-        commit_git = "git rev-parse HEAD"
-        commit_hg = "hg id -i"
-
-        if(not (os.path.isdir(repo_dir))):
-            print("Missing:" + repo_dir)
-            print("Executing: " + clone_git)
-            code, out, err = get_exitcode_stdout_stderr(clone_git)
-            print (code, out, err)
-            if (code != 0):
-                print("Executing: " + clone_git)
-            code, out, err = get_exitcode_stdout_stderr(clone_hg)
-            print(code, out, err)
-
-        with cd (repo_dir):
-            #once in the repo, pull latest commit from desired branch
-            code, out, err = get_exitcode_stdout_stderr(pull_git)
-            if (code != 0):
-                code, out, err = get_exitcode_stdout_stderr(pull_hg)
-
-            #once we have the latest code, get the commit ID
-            code, out, err = get_exitcode_stdout_stderr(commit_git)
-            if (code != 0):
-                code, out, err = get_exitcode_stdout_stderr(commit_hg)
-        return out
-
-def create_conda_env(project, dependencies):
-    """
-    Create a conda env and pip install the latest local reqs into it.
+    Create a conda env.
     """
     timestr = time.strftime("%Y%m%d-%H%M%S")
     env_name = project + "_" + timestr
-    conda_create = "conda create -y -n " + env_name + " python=2.7 pip"
+    conda_create = "conda create -y -n " + env_name + " python=2.7 pip numpy scipy swig"
     code, out, err = get_exitcode_stdout_stderr(conda_create)
     if (code == 0):
         return env_name
     else:
         print("Falied to create conda environment", env_name)
 
+
 def activate_install_conda_env(env_name, dependencies):
     """
     Activate an existing conda env; install dependencies into it
     """
     #screwing with path instead of activating.
-    print("PATH AT FIRST:"+ os.environ["PATH"])
+    print("PATH AT FIRST:" + os.environ["PATH"])
     path = os.environ["PATH"].split(os.pathsep)
-    os.environ["KEEP_PATH"]= path[0] #old path leader
-    path.remove(path[0]) #remove default conda env
+    os.environ["KEEP_PATH"] = path[0]  # old path leader
+    path.remove(path[0])  # remove default conda env
     path = (os.pathsep).join(path)
-    path = (os.path.expanduser("~") + "/anaconda/envs/" + env_name +"/bin") + os.pathsep +  path
-    print ("PATH NOW: "+ path)
+    path = (os.path.expanduser("~") + "/anaconda/envs/" + env_name + "/bin") + os.pathsep +  path
+    print ("PATH NOW: " + path)
     os.environ["PATH"] = path
 
     #install a couple things that must be in there
-    get_exitcode_stdout_stderr("pip install numpy")
-    get_exitcode_stdout_stderr("pip install scipy")
-    get_exitcode_stdout_stderr("pip install mercurial")
-    get_exitcode_stdout_stderr("pip install git+https://github.com/naylor-b/testflo.git")
+    # get_exitcode_stdout_stderr("conda install numpy")
+    # get_exitcode_stdout_stderr("conda install scipy")
+    # get_exitcode_stdout_stderr("pip install mercurial")
+    get_exitcode_stdout_stderr("pip install git+https://github.com/swryan/testflo@work")
 
+    install_cmd = "python setup.py install"
     for dependency in dependencies:
-        git_install = "pip install git+" + dependency
-        hg_install = "pip install hg+" + dependency
-
-        print("\nInstalling dependency: " + dependency)
-        print("Trying git: " + git_install)
-        code, out, err = get_exitcode_stdout_stderr(git_install)
-        print (code, out, err)
-        if (code != 0):
-            print("Trying hg: " + hg_install)
-            code, out, err = get_exitcode_stdout_stderr(hg_install)
+        with repo(dependency):
+            code, out, err = get_exitcode_stdout_stderr(install_cmd)
             print (code, out, err)
-        print ("\n\n")
 
-    #finally, install the actual project into the conda env
-    get_exitcode_stdout_stderr("pip install -e .")
 
 def remove_conda_env(env_name):
     """
     Deactivate and remove a conda env at the end of a benchmarking run.
     """
-    print("PATH AT FIRST: "+ os.environ["PATH"])
+    print("PATH AT FIRST: " + os.environ["PATH"])
     path = os.environ["PATH"].split(os.pathsep)
-    path.remove(path[0]) #remove modified
+    path.remove(path[0])  # remove modified
     path = (os.pathsep).join(path)
     path = ((os.environ["KEEP_PATH"]) + os.pathsep +  path)
     print ("PATH NOW: " + path)
     os.environ["PATH"] = path
 
     conda_delete = "conda env remove -y --name " + env_name
-    #code, out, err = get_exitcode_stdout_stderr(conda_delete)
-    #return code
+    code, out, err = get_exitcode_stdout_stderr(conda_delete)
+    return code
 
-def run_testflo_benchmarks():
+
+def run_benchmarks():
     """
-    Run testflo (eventually to run benchmarks)
+    Use testflo to run benchmarks)
     """
-    testflo_cmd = "testflo "
+    testflo_cmd = "testflo -bv"
     code, out, err = get_exitcode_stdout_stderr(testflo_cmd)
     print (code, out, err)
 
-def read_benchmark_data(cursor):
-    """
-    Stolen directly from Steve, needs to be modified
-    """
-    benchmarks = {}
-    with open('benchmark_data.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            spec = row[1].rsplit('.', 1)[1]
-            benchmarks.setdefault(spec, {})
-            benchmarks[spec].setdefault('timestamp', []).append(row[0])
-            benchmarks[spec].setdefault('status', []).append(row[2])
-            benchmarks[spec].setdefault('elapsed', []).append(row[3])
-            benchmarks[spec].setdefault('maxrss', []).append(row[4])
-    from pprint import pprint
-    pprint(benchmarks)
-    #eventually do something with database cursor to update entries
+
+def _get_parser():
+    """Returns a parser to handle command line args."""
+
+    parser = ArgumentParser()
+    parser.usage = "benchmark [options]"
+
+    parser.add_argument('projects', metavar='project', nargs='*',
+                        help='project to benchmark, pointing to a JSON file in the current directory')
+
+    return parser
 
 
-if __name__ == "__main__":
-    #projects = ["OpenMDAO", "pointer", "mission-allocation", "CADRE"]
-    projects = ["OpenMDAO"]
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
 
-    for project in projects:
-        benchmark(project)
+    # update configuration per cfg file
+    conf.update(read_json("benchmark.cfg"))
+
+    options = _get_parser().parse_args(args)
+
+    with cd(conf["working_dir"]):
+        for project in options.projects:
+            benchmark(project)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
