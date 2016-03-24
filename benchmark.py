322f69879302db133cc48ff5ae09d80df1f22354
kmarsteller <Keith.G.Marsteller@nasa.gov>
2016-03-24 10:23:16 -0400

    adding timestamp to x-axis

--- benchmark.py
+++ benchmark.py
@@ -13,7 +13,7 @@ import time
 import json
 import csv
 import math
-
+import datetime
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -519,8 +519,9 @@ class BenchmarkDatabase(object):
             fig, a1 = pyplot.subplots()
             a1.get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
             #x = np.array(range(len(timestamp)))
-            print(datetime.datetime.fromtimestamp(data['timestamp']).strftime('%Y-%m-%d %H:%M:%S'))
-            x = datetime.datetime.fromtimestamp(data['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+            x = []
+            for stamp in data['timestamp']:
+                x.append(datetime.datetime.fromtimestamp(float(stamp)).strftime('%Y-%m-%d %H:%M:%S'))
 
             a1.plot(x, elapsed, 'b-')
             a1.set_xlabel('run_date')
