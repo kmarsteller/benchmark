7e061274125a3e3da5c56d7061528402e5638e5f
Keith Marsteller <Keith.G.Marsteller@nasa.gov>
2016-05-26 10:04:59 -0400

    fix-up

--- benchmark.py
+++ benchmark.py
@@ -279,8 +279,11 @@ def activate_env(env_name, dependencies, local_repos):
 
     logging.info("env_name: %s, path: %s", env_name, env["PATH"])
 
+    #need to do a pip install with --prefix to get things installed into proper conda env
+    pipinstall = "pip install --install-option=\"--prefix=" + conda_dir.replace("bin",  "envs/"+env_name) + "\" "
+
     # install testflo to do the benchmarking
-    code, out, err = get_exitcode_stdout_stderr("pip install --install-option=\"--prefix=" + conda_dir.replace("bin",  "envs/"+env_name) + "\" git+https://github.com/naylor-b/testflo")
+    code, out, err = get_exitcode_stdout_stderr( pipinstall + "git+https://github.com/naylor-b/testflo")
     if (code != 0):
         raise RuntimeError("Failed to install testflo to", env_name, code, out, err)
 
@@ -288,7 +291,7 @@ def activate_env(env_name, dependencies, local_repos):
     for dependency in dependencies:
         # numpy and scipy are installed when the env is created
         if (not dependency.startswith("python=") and not dependency.startswith("numpy") and not dependency.startswith("scipy")):
-            code, out, err = get_exitcode_stdout_stderr("pip install --install-option=\"--prefix=" + conda_dir.replace("bin",  "envs/"+env_name) + "\" " + os.path.expanduser(dependency))
+            code, out, err = get_exitcode_stdout_stderr(pipinstall + os.path.expanduser(dependency))
             if (code != 0):
                 raise RuntimeError("Failed to install", dependency, "to", env_name, code, out, err)
 
@@ -466,7 +469,9 @@ class BenchmarkDatabase(object):
                 logging.info('INSERTING BenchmarkData %s' % str(row))
                 try:
                     spec = row[1].rsplit('/', 1)[1]  # remove path from benchmark file name
-                    self.cursor.execute("INSERT INTO BenchmarkData VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (row[0], spec, row[2], float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])))
+                    #self.cursor.execute("INSERT INTO BenchmarkData VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (row[0], spec, row[2], float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7])))
+                    self.cursor.execute("INSERT INTO BenchmarkData VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (row[0], spec, row[2], float(row[3]), float(row[4]), 0.0, 0.0, 0.0)
+
                     data_added = True
                 except IndexError:
                     print("Invalid benchmark specification found in results:\n %s" % str(row))
@@ -528,7 +533,7 @@ class BenchmarkDatabase(object):
                 data.setdefault('LoadAvg1m', []).append(row[5])
                 data.setdefault('LoadAvg5m', []).append(row[6])
                 data.setdefault('LoadAvg15m', []).append(row[7])
-  		  
+
             if not data:
                 logging.warn("No data to plot for %s", spec)
                 print("No data to plot for %s" % spec)
