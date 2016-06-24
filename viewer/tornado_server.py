c97561eb1f7905526eb9fb534b8cfcbfc04cb916
swryan <stephen.w.ryan@gmail.com>
2016-06-23 21:08:59 -0400

    messing with commits

--- viewer/tornado_server.py
+++ viewer/tornado_server.py
@@ -5,8 +5,8 @@ import tornado.ioloop
 import tornado.web
 from benchmark import BenchmarkDatabase
 
-#database_dir = os.path.abspath(os.path.dirname(__file__))
-database_dir = "/home/openmdao/webapps/benchmark_data_server/"
+database_dir = os.path.abspath(os.path.dirname(__file__))
+# database_dir = "/home/openmdao/webapps/benchmark_data_server/"
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -14,17 +14,19 @@ class MainHandler(tornado.web.RequestHandler):
         print "==> MainHandler:", self.request.uri
         self.write(self.request.uri)
 
+
 class ProjectHandler(tornado.web.RequestHandler):
     def get(self, project):
         print "==> ProjectHandler:", project
         self.write("Project: "+project)
+
 
 class SpecHandler(tornado.web.RequestHandler):
     def get(self, project, spec):
         print "==> SpecHandler:", project, spec
         print os.path.join(database_dir, project)
         db = BenchmarkDatabase(os.path.join(database_dir, project))
-        
+
         data = {}
         for row in db.cursor.execute('SELECT * FROM BenchmarkData WHERE Spec=? and Status=="OK" ORDER BY DateTime', (spec,)):
             data.setdefault("timestamp", []).append(row[0])
@@ -34,34 +36,38 @@ class SpecHandler(tornado.web.RequestHandler):
             data.setdefault("LoadAvg1m", []).append(row[5])
             data.setdefault("LoadAvg5m", []).append(row[6])
             data.setdefault("LoadAvg15m", []).append(row[7])
-        
-	#not sure how to do this properly
-        #for row in db.cursor.execute('SELECT CommitID FROM Commits WHERE DateTime==? ORDER BY DateTime', ()):
-	#    data.setdefault("commit", []).append(row[8])
+
+        commits = []
+
+        for timestamp in data['timestamp']:
+            tmp_list = []
+            for row in db.cursor.execute('SELECT * FROM Commits WHERE DateTime==? ORDER BY DateTime', (timestamp,)):
+                # row[0] is timestamp, row[1] is trigger, row[2] is commit
+                name = row[1].rsplit('/', 1)[1]
+                link = (row[1]+'/commit/'+row[2]).strip().replace(':', '/').replace('git@', 'http://')
+                tmp_list.append((name, link))
+            commits.append(tmp_list)
+
+        data['commits'] = commits
+
+        from pprint import pprint
+        pprint(data['timestamp'])
+        pprint(data['commits'])
 
         # NOTE: this dictionary is probably not the best data structure to use here
         #       (depending on the ultimate means of rendering it to HTML)
 
         if not data:
             print("No data to plot for %s" % spec)
-            response = "No data for %s %s " % (project, spec)
+            return "No data for %s %s " % (project, spec)
         else:
-            response = "{:s}: {:s}\n".format(project, spec)
-            line_fmt = "{:s} {:4s} {:7.2f} {:7.2f} {:7.2f} {:7.2f} {:7.2f}\n"
-            for i in range(len(data["timestamp"])):
-                response += line_fmt.format(            
-                    str(datetime.fromtimestamp(data["timestamp"][i])),
-                    data["status"][i],
-                    data["elapsed"][i],
-                    data["memory"][i],
-                    data["LoadAvg1m"][i],
-                    data["LoadAvg5m"][i],
-                    data["LoadAvg15m"][i]
-                )
             bench_title = "Benchmark Results for " + spec
+
             def date(timestamp):
-		return str(datetime.fromtimestamp(timestamp)) 
-        self.render("template.html", title=bench_title, items=data, date=date)
+                return str(datetime.fromtimestamp(timestamp))
+
+            self.render("template.html", title=bench_title, items=data, date=date)
+
 
 def make_app():
     return tornado.web.Application([
