de52de9705e0d15ab4c7fe076c6b77514eedfdd6
Keith Marsteller <Keith.G.Marsteller@nasa.gov>
2016-06-29 17:21:34 -0400

    adding some formatting options for project template, git diff for spec template, etc.

--- viewer/tornado_server.py
+++ viewer/tornado_server.py
@@ -5,8 +5,8 @@ import tornado.ioloop
 import tornado.web
 from benchmark import BenchmarkDatabase
 
-#database_dir = os.path.abspath(os.path.dirname(__file__))
-database_dir = "/home/openmdao/webapps/benchmark_data_server/"
+database_dir = os.path.abspath(os.path.dirname(__file__))
+#database_dir = "/home/openmdao/webapps/benchmark_data_server/"
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -18,7 +18,7 @@ class MainHandler(tornado.web.RequestHandler):
             print "    "+file
             dbs.append(file.lsplit(".")[0])
         self.render("main_template.html", dbs=dbs)
-        
+
 
 class ProjectHandler(tornado.web.RequestHandler):
     def get(self, project):
@@ -26,13 +26,19 @@ class ProjectHandler(tornado.web.RequestHandler):
         print os.path.join(database_dir, project)
         if (project+".db") not in os.listdir(database_dir):
             print "      PROJECT " + project + " DOES NOT EXIST."
-            return            
+            return
         db = BenchmarkDatabase(os.path.join(database_dir, project))
-        specs=[]
-        for row in db.cursor.execute('SELECT * FROM BenchmarkData WHERE Status=="OK" ORDER BY DateTime'):
-            if (row[1] not in specs):
-              specs.append(row[1])
-        self.render("proj_template.html", title=project, spec=specs)
+        specs = db.get_specs()
+        dates = []
+        for spec in specs:
+            for row in db.cursor.execute('SELECT DateTime FROM BenchmarkData WHERE Spec==? ORDER BY DateTime DESC LIMIT 1', (spec,)):
+                dates.append(row[0])
+
+        def date(timestamp):
+            return str(datetime.fromtimestamp(timestamp))
+
+        self.render("proj_template.html", title=project, spec=specs, date=date, dates=dates)
+
 
 class SpecHandler(tornado.web.RequestHandler):
     def get(self, project, spec):
@@ -50,24 +56,24 @@ class SpecHandler(tornado.web.RequestHandler):
             data.setdefault("LoadAvg5m", []).append(row[6])
             data.setdefault("LoadAvg15m", []).append(row[7])
 
-        commits = []
-        for timestamp in data['timestamp']:
+        commits = {}
+        for timestamp in data["timestamp"]:
             tmp_list = []
             for row in db.cursor.execute('SELECT * FROM Commits WHERE DateTime==? ORDER BY DateTime', (timestamp,)):
                 # row[0] is timestamp, row[1] is trigger, row[2] is commit
                 name = row[1].rsplit('/', 1)[1]
-                link = (row[1]+'/commit/'+row[2]).strip().replace(':', '/').replace('git@', 'http://')
-                tmp_list.append((name, link))
-            commits.append(tmp_list)
+                #link = (row[1]+'/commit/'+row[2]).strip().replace(':', '/').replace('git@', 'http://')
+                commit = row[2]
+                tmp_list.append((name, commit))
+            commits[timestamp] = tmp_list
+
+        #     # for row in db.cursor.execute('SELECT * FROM Commits WHERE DateTime==(SELECT MIN(DateTime) FROM Commits WHERE DateTime < ?) ORDER BY DateTime', (timestamp,)):
 
         data['commits'] = commits
 
         from pprint import pprint
         pprint(data['timestamp'])
         pprint(data['commits'])
-
-        # NOTE: this dictionary is probably not the best data structure to use here
-        #       (depending on the ultimate means of rendering it to HTML)
 
         if not data:
             print("No data to plot for %s" % spec)
