bd01f0f2ff7b948ee781062c6e72e9b621f8fc32
Stephen Ryan <stephen.w.ryan@gmail.com>
2016-07-14 10:38:52 -0400

    make project name a link in trigger message check tha DB exists in spec_handler

--- viewer/tornado_server.py
+++ viewer/tornado_server.py
@@ -43,42 +43,47 @@ class SpecHandler(tornado.web.RequestHandler):
         """
         Display history for specific benchmark.
         """
-        db = BenchmarkDatabase(os.path.join(database_dir, project))
+        dbs = [f for f in os.listdir(database_dir) if f.endswith(".db")]
 
-        data = {}
-        for row in db.cursor.execute('SELECT * FROM BenchmarkData WHERE Spec=? and Status=="OK" ORDER BY DateTime', (spec,)):
-            data.setdefault("timestamp", []).append(row[0])
-            data.setdefault("status", []).append(row[2])
-            data.setdefault("elapsed", []).append(row[3])
-            data.setdefault("memory", []).append(row[4])
-            data.setdefault("LoadAvg1m", []).append(row[5])
-            data.setdefault("LoadAvg5m", []).append(row[6])
-            data.setdefault("LoadAvg15m", []).append(row[7])
-
-        commits = {}
-        for timestamp in data["timestamp"]:
-            tmp_list = []
-            for row in db.cursor.execute('SELECT * FROM Commits WHERE DateTime==? ORDER BY DateTime', (timestamp,)):
-                # row[0] is timestamp, row[1] is trigger, row[2] is commit
-                prefix = ""
-                name = row[1].rsplit('/', 1)[1]
-                if "github" in row[1]:
-                    prefix = row[1].strip().replace(':', '/').replace('git@', 'http://')
-                if "bitbucket" in row[1]:
-                    prefix = row[1]
-                commit = row[2]
-                tmp_list.append((name, commit, prefix))
-            commits[timestamp] = tmp_list
-
-        data["commits"] = commits
-        data["datestr"] = ["{:%Y-%m-%d %H:%M}".format(datetime.fromtimestamp(t)) for t in data["timestamp"]]
-
-        if not data:
-            print("No data to plot for %s" % spec)
-            return "No data for %s %s " % (project, spec)
+        if (project+".db") not in dbs:
+            self.finish("<html><body>%s is not a valid project</body></html>" % project)
         else:
-            bench_title = "Benchmark Results for " + spec
-            self.render("spec_template.html", title=bench_title, items=data)
+            db = BenchmarkDatabase(os.path.join(database_dir, project))
+
+            data = {}
+            for row in db.cursor.execute('SELECT * FROM BenchmarkData WHERE Spec=? and Status=="OK" ORDER BY DateTime', (spec,)):
+                data.setdefault("timestamp", []).append(row[0])
+                data.setdefault("status", []).append(row[2])
+                data.setdefault("elapsed", []).append(row[3])
+                data.setdefault("memory", []).append(row[4])
+                data.setdefault("LoadAvg1m", []).append(row[5])
+                data.setdefault("LoadAvg5m", []).append(row[6])
+                data.setdefault("LoadAvg15m", []).append(row[7])
+
+            commits = {}
+            for timestamp in data["timestamp"]:
+                tmp_list = []
+                for row in db.cursor.execute('SELECT * FROM Commits WHERE DateTime==? ORDER BY DateTime', (timestamp,)):
+                    # row[0] is timestamp, row[1] is trigger, row[2] is commit
+                    prefix = ""
+                    name = row[1].rsplit('/', 1)[1]
+                    if "github" in row[1]:
+                        prefix = row[1].strip().replace(':', '/').replace('git@', 'http://')
+                    if "bitbucket" in row[1]:
+                        prefix = row[1]
+                    commit = row[2]
+                    tmp_list.append((name, commit, prefix))
+                commits[timestamp] = tmp_list
+
+            data["commits"] = commits
+            data["datestr"] = ["{:%Y-%m-%d %H:%M}".format(datetime.fromtimestamp(t)) for t in data["timestamp"]]
+
+            if not data:
+                print("No data to plot for %s" % spec)
+                return "No data for %s %s " % (project, spec)
+            else:
+                bench_title = "Benchmark Results for " + spec
+                self.render("spec_template.html", title=bench_title, items=data)
 
 
 if __name__ == "__main__":
